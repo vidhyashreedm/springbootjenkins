ba4616c383c37731ae97e88ea798bfdeafa4ec5e
ravindratf <ravindra.rangashamaiah@thoughtfocus.com>
2024-12-14 18:06:57 -0500

    fourth commit

--- src/test/java/com/nt/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/nt/SpringbootJenkinsApplicationTests.java
@@ -1,8 +1,8 @@
 package com.nt;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
